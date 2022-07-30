ecb48b0435cc1f2b0d6e5255f7c55818613c4416
alirezax4 <alirezaxmz@gmail.com>
2022-07-30 18:20:17 +0430

    create crud methods in UserRepository(create,read,update,delete)

--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -1,4 +1,31 @@
 package Repository;
 
+import Entity.User;
+
 public class UserRepository {
+
+    public User findUserById(int id){
+        return null;
+
+    }
+    public void createUser(User user){
+
+    }
+
+    public void deleteUser(int id){
+
+    }
+
+    public void updateUser(int id , User user){
+
+    }
+
+    public User[] findAllUser(){
+        return null;
+
+    }
+
+    public boolean isUsernameExist(String username){
+        return false;
+    }
 }
