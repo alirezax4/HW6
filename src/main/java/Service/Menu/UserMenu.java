cb947f620e74aa7c9d30c73f1567903ba77dbe71
alirezax4 <alirezaxmz@gmail.com>
2022-07-30 20:13:11 +0430

    create get input and login and sign up methods

--- src/main/java/Service/Menu/UserMenu.java
+++ src/main/java/Service/Menu/UserMenu.java
@@ -1,4 +1,21 @@
 package Service.Menu;
 
+import Service.ApplicationConstant;
+import Service.Printer;
+
 public class UserMenu {
+
+    public void getInput(){
+        System.out.println("WELCOME");
+        while(true){
+
+        }
+    }
+
+    public void login(){
+
+    }
+    public void signUp(){
+
+    }
 }
