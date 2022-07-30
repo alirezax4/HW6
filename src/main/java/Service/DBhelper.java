e68f595cc45edc7a56bf0f67b57c34b4248772fd
alirezax4 <alirezaxmz@gmail.com>
2022-07-30 20:10:16 +0430

    complete connect to database

--- src/main/java/Service/DBhelper.java
+++ src/main/java/Service/DBhelper.java
@@ -1,4 +1,34 @@
 package Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBhelper {
+
+    private final static String SERVER_ADDRESS = "127.0.0.1:5432";
+    private final static String DBNAME = "HW6";
+    private final static String USER = "alireza";
+    private final static String PASSWORD = "Alix0990#";
+
+    private Connection connection ;
+
+
+
+    public Connection Connect (){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://"+SERVER_ADDRESS+"/"+DBNAME,USER,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
+
+
 }
