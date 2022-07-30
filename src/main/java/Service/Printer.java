11571224701866d414dd0b0d3c865d30930a33d5
alirezax4 <alirezaxmz@gmail.com>
2022-07-30 19:06:11 +0430

    add method print for show menu

--- src/main/java/Service/Printer.java
+++ src/main/java/Service/Printer.java
@@ -1,4 +1,11 @@
 package Service;
 
 public class Printer {
+    public static void print(String[] menu){
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i+1+ " " + menu[i]);
+
+            
+        }
+    }
 }
