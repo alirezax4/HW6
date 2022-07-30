0069361dce8b5d2e6df016a4efddc3a89c8a15a1
alirezax4 <alirezaxmz@gmail.com>
2022-07-30 20:11:26 +0430

    create get connection by new object in connection class

--- src/main/java/Service/ApplicationConstant.java
+++ src/main/java/Service/ApplicationConstant.java
@@ -1,4 +1,12 @@
 package Service;
 
+import java.sql.Connection;
+
 public class ApplicationConstant {
+
+    private static Connection connection = new DBhelper().Connect();
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
